642e4af00d99e662c6da27eca141438cc959f2fb
mahfuz-antor <antorcnj2014@gmail.com>
2021-02-09 23:31:27 +0600

    submit final file

--- script.js
+++ script.js
@@ -1,50 +1,50 @@
-const searchSongs = () => {
+const searchFoods = () => {
     const textSearch = document.getElementById('search-field').value;
     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${textSearch}`
     fetch(url)
         .then(res => res.json())
-        .then(data => displaySongs(data.meals))
+        .then(data => displayFoods(data.meals))
 }
 
-const displaySongs = songs => {
-    const songContainer = document.getElementById('song-container');
-    songContainer.innerHTML = ''
+const displayFoods = Foods => {
+    const foodContainer = document.getElementById('food-container');
+    foodContainer.innerHTML = ''
 
-    songs.forEach(song => {
-        // console.log(song)
-        const songDiv = document.createElement('div');
-        songDiv.className = 'single-result row align-items-center my-3 p-3'
-        songDiv.innerHTML = `
-        <div class="col-md-10">
-        <img src="${song.strMealThumb}">
-         <h3 class="lyrics-name">${song.strMeal}</h3>
-        <p class="author lead">${song.strCategory}</p>
+    Foods.forEach(food => {
+        // console.log(food)
+        const foodDiv = document.createElement('div');
+        foodDiv.className = 'extra-part single-result'
+        foodDiv.innerHTML = `
+        <div onclick="getLyric('${food.idMeal}')" class=" col-md-10">
+        <img src="${food.strMealThumb}">
+         <h1 class="element-name">${food.strMeal}</h1>
+        <p class="author lead">${food.strCategory}</p>
          <div class="col-md-3 text-md-right text-center ">
-         <button onclick="getLyric('${song.idMeal}')" class="btn btn-success ">Details</button>
         </div>
         </div>
         `;
-        songContainer.appendChild(songDiv);
+        foodContainer.appendChild(foodDiv);
     })
 }
 
-const getLyric = name => {
-    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${name}`
+const getLyric = element => {
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${element}`
     fetch(url)
-    .then(res => res.json())
-    .then(data => displayLyrics(data.meals[0]));
+        .then(res => res.json())
+        .then(data => displayElement(data.meals[0]));
 }
 
-const displayLyrics = meal => {
-    const div = document.getElementById('song-lyrics');
+const displayElement = meal => {
+    const div = document.getElementById('food-element');
     div.innerHTML = `
     <img src="${meal.strMealThumb}">
-    <p>${meal.strIngredient1}</p>
-    <p>${meal.strIngredient2}</p>
-    <p>${meal.strIngredient3}</p>
-    <p>${meal.strIngredient4}</p>
-    <p>${meal.strIngredient5}</p>
-    <p>${meal.strIngredient6}</p>
+    <p>&#9758 ${meal.strIngredient1}</p>
+    <p>&#9758 ${meal.strIngredient2}</p>
+    <p>&#9758 ${meal.strIngredient3}</p>
+    <p>&#9758 ${meal.strIngredient4}</p>
+    <p>&#9758 ${meal.strIngredient5}</p>
+    <p>&#9758 ${meal.strIngredient6}</p>
+    <p>&#9758 ${meal.strIngredient7}</p>
     `;
     div.appendChild(meal);
 }
